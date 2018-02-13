c8da79a3de4822dc40e58c31dc46230d5bcf4151
pLOPeGG <douzont@gmail.com>
2018-02-13 08:46:51 +0100

    State0 & Symbol (EXPR)

--- TP1 Grammaire Langage/State0.cpp
+++ TP1 Grammaire Langage/State0.cpp
@@ -10,3 +10,22 @@ State0::State0()
 State0::~State0()
 {
 }
+
+bool State0::transition(Automate& automate, Symbole* symbole)
+{
+	switch (*symbole)
+	{
+	case INT:
+		automate.decalage(symbole, new State0);
+		break;
+	case OPENPAR:
+		automate.decalage(symbole, new State2);
+		break;
+	case EXPR:
+		automate.decalage(symbole, new State1);
+		break;
+	default:
+		automate.decalage(symbole, new StateError);
+
+	}
+}
--- TP1 Grammaire Langage/State0.h
+++ TP1 Grammaire Langage/State0.h
@@ -2,9 +2,13 @@
 #include "State.h"
 class State0 : public State {
 public:
-	State0();
+	State0()
+	{
+		name = "Etat 0";
+	}
 	virtual ~State0();
 
 	bool transition(Automate& automate, Symbole* symbole);
+
 };
 
